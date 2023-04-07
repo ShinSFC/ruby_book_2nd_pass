b54669a88d9f158ff15b8817aff73d2d8be29e89
ShinpeiH <dctechsf@gmail.com>
2023-04-06 18:51:54 -0700

    added loops and iterators

--- example_codes.rb
+++ example_codes.rb
@@ -224,3 +224,54 @@ end
 loop do
   puts "This will keep printing until you hit Ctrl + c"
 end
+
+# useful_loop.rb
+
+i = 0
+loop do
+  i = i + 1
+  puts i
+  break         # this will cause execution to exit the loop
+end
+
+# conditional_loop.rb
+
+i = 0
+loop do
+  i = i + 2
+  puts i
+  if i == 10
+    break       # this will cause execution to exit the loop
+  end
+end
+
+# next_loop.rb
+
+i = 0
+loop do
+  i = i + 2
+  if i == 4
+    next        # skip rest of the code in this iteration
+  end
+  puts i
+  if i == 10
+    break
+  end
+end
+
+
+# variable scope in loops:
+
+loop do
+  x = 42
+  break
+end
+puts x     # Raises an error -- x is not in scope outside of the block
+
+x = 42
+loop do
+  puts x   # Prints 42 -- x is in scope inside the block
+  x = 2    # We can even change the value of x
+  break
+end
+puts x     # 2 -- the value was changed
