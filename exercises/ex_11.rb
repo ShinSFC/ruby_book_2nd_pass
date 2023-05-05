fafec546c28de6da4c128d39b2773b4028979ef2
ShinpeiH <dctechsf@gmail.com>
2023-05-04 17:03:45 -0700

    added more exercises

--- exercises/ex_11.rb
+++ exercises/ex_11.rb
@@ -2,5 +2,8 @@ contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"], ["sally@email
 
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
 
-contacts = {"Joe Smith" => {contact_data[1]}, "Sally Johnson"
+contacts = {"Joe Smith" => {email: contact_data[0][0], address: contact_data[0][1], phone: contact_data[0][2]},
+  "Sally Johnson" => {email: contact_data[1][0], address: contact_data[1][1], phone: contact_data[1][2]}}
+
+p contacts
 
